9410eb7fb535af326869baade062a07b19a44208
Frank A. Krueger <fak@praeclarum.org>
2023-07-12 19:27:28 -0700

    Add cameras to reconstruction

--- ColmapSharp/Colmap.cs
+++ ColmapSharp/Colmap.cs
@@ -10,11 +10,24 @@ public enum Quality { Low, Medium, High, Extreme }
 
 /// <summary>
 /// Represents a single image in the sparse reconstruction.
-///
-/// The reconstructed pose of an image is specified as the projection from world to the camera coordinate system of an image using a quaternion (QW, QX, QY, QZ) and a translation vector (TX, TY, TZ).
-/// The quaternion is defined using the Hamilton convention, which is, for example, also used by the Eigen library.
-/// The coordinates of the projection/camera center are given by -R^t * T, where R^t is the inverse/transpose of the 3x3 rotation matrix composed from the quaternion and T is the translation vector.
-/// The local camera coordinate system of an image is defined in a way that the X axis points to the right, the Y axis to the bottom, and the Z axis to the front as seen from the image.
+/// <para>
+/// The reconstructed pose of an image is specified as the projection
+/// from world to the camera coordinate system of an image
+/// using a quaternion (QW, QX, QY, QZ) and a translation vector (TX, TY, TZ).
+/// </para>
+/// <para>
+/// The quaternion is defined using the Hamilton convention,
+/// which is, for example, also used by the Eigen library.
+/// </para>
+/// <para>
+/// The coordinates of the projection/camera center are given by -R^t * T,
+/// where R^t is the inverse/transpose of the 3x3 rotation matrix
+/// composed from the quaternion and T is the translation vector.
+/// </para>
+/// <para>
+/// The local camera coordinate system of an image is defined in a way that
+/// the X axis points to the right, the Y axis to the bottom, and the Z axis to the front as seen from the image.
+/// </para>
 /// </summary>
 public record Image(int Id, double QW, double QX, double QY, double QZ, double TX, double TY, double TZ, int CameraId, string Name) {
     public static Image? ReadLine(string line) {
@@ -58,10 +71,50 @@ public record Image(int Id, double QW, double QX, double QY, double QZ, double T
 }
 
 public record Camera(int Id, string Model, int Width, int Height, double[] Params) {
+    public static Camera? ReadLine(string line) {
+        var icult = System.Globalization.CultureInfo.InvariantCulture;
+        if (string.IsNullOrWhiteSpace(line)) {
+            return null;
+        }
+        var parts = line.Split(' ');
+        if (parts.Length < 4) {
+            return null;
+        }
+        if (parts[0][0] == '#') {
+            return null;
+        }
+        return new Camera(
+            int.Parse(parts[0]),
+            parts[1],
+            int.Parse(parts[2]),
+            int.Parse(parts[3]),
+            parts[4..].Select(p => double.Parse(p, icult)).ToArray()
+        );
+    }
+    public static Camera[] ReadCamerasFromFile(string filePath) {
+        var lines = File.ReadAllLines(filePath);
+        var cameras = new List<Camera>();
+        for (int i = 0; i < lines.Length; i++) {
+            string? line = lines[i];
+            var camera = ReadLine(line);
+            if (camera != null) {
+                cameras.Add(camera);
+            }
+        }
+        return cameras.ToArray();
+    }
 }
 
-public record Reconstruction(Image[] Images) {
-
+public record Reconstruction(Image[] Images, Camera[] Cameras) {
+    public Camera? FindCamera(int id) {
+        foreach (var camera in Cameras) {
+            if (camera.Id == id) {
+                return camera;
+            }
+        }
+        return null;
+    }
+    public Camera GetCamera(int id) => FindCamera(id) ?? throw new Exception($"Camera {id} not found");
 }
 
 public static class Colmap
@@ -76,8 +129,10 @@ public static class Colmap
             throw new ColmapException(r);
         }
         var imagesPath = Path.Combine(workDirectory, "sparse", "0", "images.txt");
+        var camerasPath = Path.Combine(workDirectory, "sparse", "0", "cameras.txt");
         var images = Image.ReadImagesFromFile(imagesPath);
-        return new Reconstruction(images);
+        var cameras = Camera.ReadCamerasFromFile(camerasPath);
+        return new Reconstruction(images, cameras);
     }
 
     [DllImport(Lib, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
