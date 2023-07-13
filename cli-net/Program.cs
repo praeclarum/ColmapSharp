40ac89fc0c55da2363f10593a30b4eb0316429ce
Frank A. Krueger <fak@praeclarum.org>
2023-07-12 17:06:45 -0700

    Build for macos

---
+++ cli-net/Program.cs
@@ -0,0 +1,9 @@
+﻿using ColmapSharp;
+using Foundation;
+
+using var w = new StreamWriter("../alog.txt");
+Console.SetOut(w);
+
+Console.WriteLine($"Hello ColmapSharp! {DateTime.Now}");
+
+// Colmap.RunAutomaticReconstruction("", "");
